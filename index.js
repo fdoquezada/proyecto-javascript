8bddc63c343d34c37373827cfd1344a0608e82b3
Fernando Quezada <fernando@debian.fernando>
2022-06-26 23:40:04 -0400

    actualizacion proyecto

--- index.js
+++ index.js
@@ -16,13 +16,40 @@ sudo apt-get install npm
 4. Instalar express para todos los usuarios del sistema
 
 sudo npm install -g express */
-
+/* 
 var http = require('http');
-var color= require('color');
+
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
+  response.end('hola fernando quezada');
 }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/'.red);
+console.log('Server running at 8081'); 
+ */
+
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hola fernando quezada')
+
+})
+
+app.get('/contacto', function (req, res) {
+  res.send('contactame en mi red social')
+
+})
+app.listen(3000)
+
+
+
+
+
+
+
+
+
+
